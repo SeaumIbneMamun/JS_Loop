29c535d55d5eac9a0332a2e982fd0c43adf51db2
Md. Seaum Ibne Mamun <94161634+SeaumIbneMamun@users.noreply.github.com>
2023-10-29 12:23:00 +0600

    Add files via upload

---
+++ a.js
@@ -0,0 +1,46 @@
+var jilapiGiven = 0;
+
+while(jilapiGiven < 6){
+    console.log('got it');
+    jilapiGiven++
+}
+
+var kane = 0;
+while( kane < 7){
+    kane++
+    console.log('kane dhora guaranteed');
+}
+
+var number = 1;
+while(number <= 10){
+    console.log(number);
+    number++ 
+   
+}
+
+var odd = 1;
+while(odd <= 15){
+    console.log(odd);
+    odd +=2
+}
+
+var even = 0;
+while (even <= 16) {
+    console.log(even);
+    even +=2
+}
+
+for (var num = 0; num <= 4; num++){
+    console.log(num);
+}
+
+for ( i=1; i<=13; i++){
+    console.log(i);
+}
+
+var arraynum = [3,4,5,6,7,8,9,10];
+
+for (var i=0; i < arraynum.length; i++) {
+    var elements = arraynum[i];
+    console.log(elements);
+}
